6e05094a2e8a057b0d10261337c4cae8d8d13a78
Jakub Tomana <jakub.tomana@protonmail.com>
2019-07-13 15:41:57 +0200

    main: replaced discord bot list library

--- discordBots.go
+++ discordBots.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"github.com/TurtleGamingFTW/dblgo-archive"
 	"github.com/bwmarrin/discordgo"
+	"github.com/kubastick/dblgo"
 	"log"
 	"time"
 )
 
 func postServerCountToDiscordBotApi(s *discordgo.Session) {
 	if config.DiscordStatsSecret != "" && config.DiscordAppID != "" {
-		dblApi := dblgo.NewDBL(config.DiscordSecret, config.DiscordAppID)
-		postStatsLoop(s, *dblApi)
+		dblApi := dblgo.NewDBLApi(config.DiscordStatsSecret)
+		postStatsLoop(s, dblApi)
 	} else {
 		log.Println("Discord Bot List Api is deactivated")
 	}
 }
 
-func postStats(s *discordgo.Session, api *dblgo.Client) {
+func postStats(s *discordgo.Session, api *dblgo.DBLApi) {
 	log.Println("Posting stats to Discord Bot List")
 	guildsNum := len(s.State.Guilds)
 	log.Printf("Number of guilds %d", guildsNum)
 
-	err := api.PostStats(guildsNum)
+	err := api.PostStatsSimple(guildsNum)
 	if err != nil {
 		log.Println("Failed to post stats to Discord Bot List", err)
 	}
 	log.Printf("Stats successfully posted")
 }
 
-func postStatsLoop(s *discordgo.Session, api dblgo.Client) {
+func postStatsLoop(s *discordgo.Session, api dblgo.DBLApi) {
 	for {
 		postStats(s, &api)
 		time.Sleep(30 * time.Minute)
--- go.mod
+++ go.mod
@@ -2,11 +2,9 @@ module MinecraftServerStatusBot
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/SpencerSharkey/gomc v0.0.0-20171215012004-e16df85c4e5f
-	github.com/TurtleGamingFTW/dblgo-archive v0.0.0-20180809235347-8287fb579843
 	github.com/bwmarrin/discordgo v0.19.0
 	github.com/fogleman/gg v1.1.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/kubastick/dblgo v0.0.0-20190713133820-efecaa639849
 	golang.org/x/image v0.0.0-20181116024801-cd38e8056d9b // indirect
 )
